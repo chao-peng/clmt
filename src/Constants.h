220f1f975c05f9c3cb71d1694153a6979e08e82d
Chao Peng <clark.chaopeng@gmail.com>
2018-02-15 10:49:41 +0000

    some new classes

--- src/Constants.h
+++ src/Constants.h
@@ -1,13 +1,15 @@
 #ifndef CLMT_CONSTANTS_HEADER_
 #define CLMT_CONSTANTS_HEADER_
 
-namespace kernel_rewriter_constants{
-    
-}
-
 namespace error_code{
     const int KERNEL_FILE_NOT_PROVIDED = -1;
     const int LINES_TO_COMPARE_NOT_VALID = -2;
+}
+
+namespace barrier_type{
+    const unsigned int LOCAL = 0x01;
+    const unsigned int GLOBAL = 0x02;
+    const unsigned int LOCAL_AND_GLOBAL = 0x03;
 }
 
 #endif
---
+++ src/OpenCLKernelInvastigator.h
@@ -0,0 +1,12 @@
+#ifndef CLMT_OPENCL_KERNEL_INVASTIGATOR_HEADER_
+#define CLMT_OPENCL_KERNEL_INVASTIGATOR_HEADER_
+
+#include <string>
+#include <map>
+
+#include "clang/Tooling/Tooling.h"
+#include "UserConfig.h"
+
+
+
+#endif
